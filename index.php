ca6cfe2b0bc767076d3c176e7f0da4843e9a9c6b
Nithin Devang <devangnithin@live.in>
2017-01-28 21:20:40 -0600

    room temp

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-require_once(dirname(__FILE__) . "/BusinessLogicLayer/accelClass.php");
-$acc = new accelClass();
-$accResult = $acc->getAllAccelData();
+require_once(dirname(__FILE__) . "/BusinessLogicLayer/tempHumClass.php");
+
+$tempHum = new tempHumClass();
+$tempHumResult = $tempHum->getAllTempData();
+$currentT = $tempHumResult[0]->temp;
+$currentH = $tempHumResult[0]->hum;
+$tempHumResult = array_reverse($tempHumResult);
+
+
 ?>
 <html>
   <head>
+      <meta http-equiv="refresh" content="5" >
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
@@ -12,17 +19,17 @@ $accResult = $acc->getAllAccelData();
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['ID', 'x-axis', 'y-axis', 'z-axis'],
+          ['ID', 'Temperature', 'Humidity'],
           <?php
-          foreach ($accResult as $accI) {
-              echo '[\''.$accI->id.'\',  '.$accI->x_val.','.$accI->y_val.', '.$accI->z_val.'],';
+          foreach ($tempHumResult as $tempHumI) {
+              echo '[\''.$tempHumI->id.'\',  '.$tempHumI->temp.','.$tempHumI->hum.'],';
               
           }
           ?>
         ]);
 
         var options = {
-          title: 'Accelerometer Data',
+          title: 'Current Room Temp',
           curveType: 'function',
           legend: { position: 'bottom' }
         };
@@ -35,5 +42,21 @@ $accResult = $acc->getAllAccelData();
   </head>
   <body>
     <div id="curve_chart" style="width: 900px; height: 500px"></div>
+    <div style="float: left; border-right: black; border-right-style: double">
+        <div>
+            <h3 style="color: green">Last Read Room Temperature:</h3>
+        </div>
+        <div>
+            <h1 style="color: green">" <?php echo $currentT; ?> &#8451; "</h1>
+        </div>
+    </div>
+    <div style="float: left">
+        <div>
+            <h3 style="color: green">Last Read Room Humidity:</h3>
+        </div>
+        <div>
+            <h1 style="color: green">" <?php echo $currentH; ?> "</h1>
+        </div>
+    </div>
   </body>
 </html>
